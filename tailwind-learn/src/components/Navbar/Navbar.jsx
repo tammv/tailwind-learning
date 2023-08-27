4eebb320c0b5ef9c8c61c40500e121885fb3db2e
Tammv <maivantam1978@gmail.com>
2023-08-27 15:31:56 +0700

    darkmode

--- tailwind-learn/src/components/Navbar/Navbar.jsx
+++ tailwind-learn/src/components/Navbar/Navbar.jsx
@@ -2,17 +2,28 @@ import { BsSunFill } from "react-icons/bs";
 import { HiOutlineMenu } from "react-icons/hi";
 import { MdOutlineClose } from "react-icons/md";
 import { useState } from "react";
+import useDarkMode from "../../useDarkMode";
+import { FaMoon } from "react-icons/fa";
+
 const Navbar = (props) => {
   const { isMobile } = props;
+  const [isDarkMode, toggleDarkMode] = useDarkMode();
   const [openMenu, setOpenMenu] = useState(false);
   const handleMenu = () => {
     setOpenMenu(!openMenu);
+  };
+  const handleDarkModeToggle = () => {
+    toggleDarkMode(!isDarkMode);
   };
   return (
     <nav className="flex item-center ">
       <div className="flex item-center">
         <div className="text-20 font-bold mr-2">Nerd card</div>
-        <BsSunFill size={"24px"} color="#e9c46a" className="cursor-pointer" />
+        {isDarkMode ? (
+          <FaMoon size={"24px"} color="#e9c46a" className="cursor-pointer" onClick={handleDarkModeToggle} />
+        ) : (
+          <BsSunFill size={"24px"} color="#e9c46a" className="cursor-pointer" onClick={handleDarkModeToggle} />
+        )}
       </div>
       <ul className="md:flex md:gap-10 ml-auto text-16 font-semibold">
         {openMenu && isMobile ? (
@@ -28,7 +39,7 @@ const Navbar = (props) => {
           </>
         )}
         {openMenu && (
-          <div className="absolute right-8 bg-white p-8 text-center text-13 text-black">
+          <div className="absolute right-8 bg-white p-8 text-center text-13 text-black z-10">
             <li className="cursor-pointer ">Features</li>
             <li className="cursor-pointer">Menu</li>
             <li className="cursor-pointer">Our Story</li>
--- tailwind-learn/src/components/Slogan/Sologan.jsx
+++ tailwind-learn/src/components/Slogan/Sologan.jsx
@@ -4,13 +4,13 @@ const Slogan = () => {
     <section className="text-36 my-14 font-extrabold relative md:my-28 md:grid md grid-cols-3 md:items-center md:text-48">
       <div className="text-center md:col-pan-1 md:col-start-2">
         <p className="">
-          Earn <span className=" text-purple-300">More</span>
+          Earn <span className=" dark:text-purple-200 text-pink-900">More</span>
         </p>
         <p className="">
-          Pay <span className=" text-purple-200">Less</span>
+          Pay <span className=" dark:text-purple-200 text-pink-900">Less</span>
         </p>
         <button className="">
-          <span className="text-20 px-6 py-[6px] font-bold mt-[27px] bg-purple-500 rounded-[14px] hover:bg-purple-400 transition-all duration-300 md:text-25 md:px-8 md:py-[8px]">
+          <span className="text-white text-20 px-6 py-[6px] font-bold mt-[27px] bg-purple-500 rounded-[14px] hover:bg-purple-400 transition-all duration-300 md:text-25 md:px-8 md:py-[8px]">
             Start
           </span>
         </button>
